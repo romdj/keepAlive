8c0f555a4da28eaf57a0827400345fa770e6a4dc
Romain Lussier <romdj_123@hotmail.com>
2024-02-23 10:55:45 +0100

    modifying script to only move mouse

--- keepAlive.go
+++ keepAlive.go
@@ -9,16 +9,19 @@ import (
 func main() {
 	for {
 		// Simulate a keyboard combination: Ctrl + Shift + P
-		robotgo.KeyToggle("ctrl", "down")
-		robotgo.KeyToggle("shift", "down")
-		robotgo.KeyTap("p")
-		robotgo.KeyToggle("shift", "up")
-		robotgo.KeyToggle("ctrl", "up")
+		// robotgo.KeyToggle("ctrl", "down")
+		// robotgo.KeyToggle("shift", "down")
+		// robotgo.KeyTap("p")
+		// robotgo.KeyToggle("shift", "up")
+		// robotgo.KeyToggle("ctrl", "up")
 
 		// Simulate a left mouse click
-		robotgo.Click("left", false)
+		robotgo.MoveRelative(120, 0)
+		robotgo.MoveRelative(0, -120)
+		robotgo.MoveRelative(-120, 0)
+		robotgo.MoveRelative(0, 120)
 
 		// Sleep for a short duration to prevent high CPU usage
-		time.Sleep(90 * time.Second)
+		time.Sleep(20 * time.Second)
 	}
 }
